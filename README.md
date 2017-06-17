01a25f0ac86d21da75127c13ded4ec13d634033e
Webb <webb@oneworldtech.net>
2017-06-17 14:46:31 +0800

    创建egg项目

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # egg-js-learn
-learn egg.js
+
+learn [ **egg.js** ](https://eggjs.org)
+
+_ 官方的教程，有一定node，koa后看，会比较容易。_  
+
+
---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1497681587673_6269';
+
+  // add your config here
+
+  return config;
+};
